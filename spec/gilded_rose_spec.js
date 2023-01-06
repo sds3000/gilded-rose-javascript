614a4717033260e8adfa4620f42043fe3a91b508
sds3000 <64442811+sds3000@users.noreply.github.com>
2023-01-06 12:07:24 -0500

    Class Paradigm
    refactoring code into class paradigm.

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,7 +1,53 @@
-describe("Gilded Rose", function() {
+describe("Update Quality", function() {
 
-  it("should do something", function() {
+  it("if sell_in is less than 0 item quality -2", function() {
+    items.push(new Item("+5 Dexterity Vest",-1,4));
     update_quality();
+    const updatedItem = items.at(-1);
+    expect (updatedItem.quality).toEqual(2);
+
   });
 
 });
+
+describe("Aged Brie", function() {
+  it("quality increases by 1 as sell in date decreases", function(){
+    items.push(new Item("Aged Brie",4,4));
+    update_quality();
+    const updatedItem = items.at(-1);
+    expect (updatedItem.quality).toEqual(5);
+  })
+  
+  
+})
+
+describe("Legendary", function() {
+  it("quality never decrease", function(){
+    items.push(new Item("Sulfuras",4,80));
+    update_quality();
+    const updatedItem = items.at(-1);
+    expect (updatedItem.quality).toEqual(80);
+  })
+})
+
+describe("Backstage passes", function() {
+  it("quality increases by 2", function(){
+    items.push(new Item("Backstage passes", 6,12));
+    update_quality();
+    const updatedItem = items.at(-1);
+    expect (updatedItem.quality).toEqual(14);
+  })
+  it("quality increases by 3", function(){
+    items.push(new Item("Backstage passes", 4,12));
+    update_quality();
+    const updatedItem = items.at(-1);
+    expect (updatedItem.quality).toEqual(15);
+  })
+  it("loses all quality", function(){
+    items.push(new Item("Backstage passes", -1,12));
+    update_quality();
+    const updatedItem = items.at(-1);
+    expect (updatedItem.quality).toEqual(0);
+  })
+})
+
--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -1,11 +1,69 @@
-function Item(name, sell_in, quality) {
-  this.name = name;
-  this.sell_in = sell_in;
-  this.quality = quality;
-}
+class Item{ // This is the item class
+  constructor(name,sell_in,qualitiy){
+    this.name = name;
+    this.sell_in = sell_in;
+    this.quality = quality;
+  };
+  updatedQuality(){
+    this.sell_in--;
+    
+  };
+};
 
+class Basic extends Item{
+  updatedQuality(){
+    this.sell_in--;
+    if(this.quality > 0){
+      this.quality--;
+    };
+    if(this.sell_in < 0 && this.quality > 0){
+      this.quality-=2;
+    }else if(this.sell_in <=0 &&this.quality > 0){
+      this.quality = 0;
+    };
+    
+  };
+};
+
+class Legendary extends Item{
+  
+  updatedQuality(){
+    this.quality = 80;
+    this.sell_in = 0;
+  };
+};
+
+class Cheese extends Item{
+  
+  updatedQuality(){
+    this.sell_in--;
+    if(this.quality < 50){
+      this.quality++;
+    };
+  };
+};
+
+class Pass extends Item{
+  updatedQuality(){
+    this.sell_in--;
+    if(this.sell_in<=10 && this.sell_in > 5){
+      this.quality+=2;
+    }else if(this.sell_in<=5 && this.sell_in >=0){
+      this.quality+=3
+    }else if(this.sell_in < 0){
+      this.quality = 0
+    } else{
+      this.quality++;
+    }
+  }
+};
+
+class Conjured extends Item{
+  
+};
+// List of items available.
 var items = []
-
+// The items being added to the items array.
 items.push(new Item('+5 Dexterity Vest', 10, 20));
 items.push(new Item('Aged Brie', 2, 0));
 items.push(new Item('Elixir of the Mongoose', 5, 7));
@@ -13,36 +71,39 @@ items.push(new Item('Sulfuras, Hand of Ragnaros', 0, 80));
 items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20));
 items.push(new Item('Conjured Mana Cake', 3, 6));
 
+// main function 
 function update_quality() {
   for (var i = 0; i < items.length; i++) {
+    // all common items degrade by 1
     if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
       if (items[i].quality > 0) {
         if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
           items[i].quality = items[i].quality - 1
         }
       }
-    } else {
-      if (items[i].quality < 50) {
+    } else { 
+      // target Aged Brie and back stage passes quality +1
+      if (items[i].quality < 50) {// 
         items[i].quality = items[i].quality + 1
-        if (items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].sell_in < 11) {
+        if (items[i].name == 'Backstage passes to a TAFKAL80ETC concert') { //if pass
+          if (items[i].sell_in < 11 && items[i].sell_in > 5) { //if between 11 days and 5 days
             if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
+              items[i].quality = items[i].quality + 1 // quality increase by +2
             }
           }
-          if (items[i].sell_in < 6) {
+          if (items[i].sell_in < 6) { // 5 days or less
             if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
+              items[i].quality = items[i].quality + 2 // quality increses by 3
             }
           }
         }
       }
     }
-    if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-      items[i].sell_in = items[i].sell_in - 1;
+    if (items[i].name != 'Sulfuras, Hand of Ragnaros') { // not legendary 
+      items[i].sell_in = items[i].sell_in - 1; // decrease sell in date by 1
     }
     if (items[i].sell_in < 0) {
-      if (items[i].name != 'Aged Brie') {
+      if (items[i].name != 'Aged Brie') { // not Aged Brie
         if (items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
           if (items[i].quality > 0) {
             if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
@@ -50,7 +111,7 @@ function update_quality() {
             }
           }
         } else {
-          items[i].quality = items[i].quality - items[i].quality
+          items[i].quality = items[i].quality - items[i].quality // backstage pass = 0 quality
         }
       } else {
         if (items[i].quality < 50) {
